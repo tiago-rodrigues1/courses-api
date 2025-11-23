6c0d03bdc8cc697beb523c7ffad79b3e8a2b65b2
tiago-rodrigues1 <tiagords127@gmail.com>
2025-11-23 19:12:00 -0300

    feat: create course entity

---
+++ src/main/java/com/ufrndata/api/domain/course/Course.java
@@ -0,0 +1,32 @@
+package com.ufrndata.api.domain.course;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "course")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Course {
+  @Id
+  @GeneratedValue
+  private UUID id;
+
+  private String code;
+  private String name;
+  private int workload;
+  private int originalPeriod;
+  private String major;
+  private boolean mandatoryToMajor;
+  private String type;
+}
